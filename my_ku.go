d32f1252b56a1df50ed07fe4f9b109df4b2371d1
Andrey Borodin <amborodin@acm.org>
2020-09-28 22:21:56 +0500

    keys unwrap

--- my_ku.go
+++ my_ku.go
@@ -6,12 +6,12 @@ import (
 )
 
 func main() {
-	var s = "64a59400000000000000000000000000"
+	var s = "8899aabbccddeeff0011223344556677fedcba98765432100123456789abcdef"
 	slice, _ := hex.DecodeString(s)
-	var fixed [16]byte
+	var fixed [32]byte
 	copy(fixed[:], slice)
-	fixed = L(fixed)
-	copy(slice,fixed[:])
+	fixed1 := Keys(fixed)[9]
+	copy(slice,fixed1[:])
 	subst := hex.EncodeToString(slice)
 	fmt.Print("L(" + s + ") = " + subst + "\n")
 }
