20e62fbbbee48c6adc8e41b3ae91c08fe181970b
Andrey Borodin <amborodin@acm.org>
2020-09-28 22:39:31 +0500

    Encryption works

--- my_ku.go
+++ my_ku.go
@@ -1,17 +1,18 @@
 package main
 
 import (
+	"bytes"
 	"encoding/hex"
 	"fmt"
 )
 
 func main() {
-	var s = "8899aabbccddeeff0011223344556677fedcba98765432100123456789abcdef"
-	slice, _ := hex.DecodeString(s)
-	var fixed [32]byte
-	copy(fixed[:], slice)
-	fixed1 := Keys(fixed)[9]
-	copy(slice,fixed1[:])
-	subst := hex.EncodeToString(slice)
-	fmt.Print("L(" + s + ") = " + subst + "\n")
+	var key = "8899aabbccddeeff0011223344556677fedcba98765432100123456789abcdef"
+	keySlice, _ := hex.DecodeString(key)
+	var data = "1122334455667700ffeeddccbbaa9988"
+	dataSlice, _ := hex.DecodeString(data)
+	ku := NewKuznets(bytes.NewReader(dataSlice), keySlice)
+	cyphered := make([]byte, 16)
+	ku.Read(cyphered)
+	fmt.Print("Ku(" + data + ") = " + hex.EncodeToString(cyphered) + "\n")
 }
